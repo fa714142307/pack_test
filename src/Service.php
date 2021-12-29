dc1082f6558cba0458011a563d620a335f28ca2d
DESKTOP-8FGUHT3\vanbal-010 <714142307@qq.com>
2021-12-29 14:31:20 +0800

    debug

---
+++ src/Service.php
@@ -0,0 +1,16 @@
+<?php
+#命名空间
+namespace Pack\Test;
+
+class Service
+{
+    public function __construct()
+    {
+    }
+
+    public function hello()
+    {
+        return 'hello world!!';
+    }
+}
+?>
